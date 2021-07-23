16931ed1bc765b11d9d4f2fb26168c8e7a6baf81
satishverma17 <30524751+satishverma17@users.noreply.github.com>
2021-07-23 18:20:53 +0200

    initial version
    initial version

---
+++ SpellChecker.py
@@ -0,0 +1,12 @@
+
+from autocorrect import spell
+
+
+def correct_sentence(line):
+    lines = line.strip().split(' ')
+    new_line = ""
+    similar_word = {}
+    for l in lines:
+        new_line += spell(l) + " "
+    # similar_word[l]=spell.candidates(l)
+    return new_line
